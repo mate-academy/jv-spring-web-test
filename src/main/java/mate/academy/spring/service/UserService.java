68aa3bb5842d9945fa9335afa5aec060f137eedc
Dasha <maliuk.dasha@gmail.com>
2022-05-05 14:23:17 +0300

    project added

---
+++ src/main/java/mate/academy/spring/service/UserService.java
@@ -0,0 +1,12 @@
+package mate.academy.spring.service;
+
+import java.util.List;
+import mate.academy.spring.model.User;
+
+public interface UserService {
+    void add(User user);
+
+    List<User> getAll();
+
+    User get(Long id);
+}
